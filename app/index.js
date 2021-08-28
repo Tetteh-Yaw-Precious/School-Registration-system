3d0e6979694c9708da2181bf8400d52e8fbff84c
Tetteh Yaw Precious <precioustetteh38@gmail.com>
2021-08-28 16:10:21 -0700

    sidenav animation done

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 const dropdownicon = document.getElementById("dropdownarrow");
 const signout = document.querySelector(".signout");
+const navopen = document.getElementById("navopen");
+const navclose = document.getElementById("navclose");
+const sidenav = document.getElementById("navbar");
+
 dropdownicon.addEventListener("click", (e) => {
   e.preventDefault;
   signout.classList.toggle("display-active");
@@ -20,10 +24,19 @@ let massPopChart = new Chart(moneyGraph, {
         label: "Population",
         data: [40, 60],
         backgroundColor: ["#13266a", "#0fb60c"],
-        fontFamily: "Raleway"
+        fontFamily: "Raleway",
       },
     ],
   },
   options: [],
 });
 
+/**opening sidenav on click */
+navopen.addEventListener("click", () => {
+  sidenav.style.animation = " slidein  0.5s  forwards";
+});
+
+/**closing sidenav on click */
+navclose.addEventListener("click", () => {
+  sidenav.style.animation = " slideout  0.5s forwards";
+});
